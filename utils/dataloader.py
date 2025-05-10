1d5443d14beab2332648c532be4bfec505c735c6
camilouribeg <128070702+camilouribeg@users.noreply.github.com>
2025-05-10 11:04:04 +1000

    Fixing some specific errors

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,8 +1,16 @@
+import torch
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from utils.caption_dataset import CaptionDataset
 
 
+# Load split image IDs
+def load_split_ids(txt_path):
+    with open(txt_path, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+# Default transforms
 def get_transforms(mode="train"):
     if mode == "train":
         return transforms.Compose(
@@ -24,6 +32,30 @@ def get_transforms(mode="train"):
         )
 
 
-def load_split_ids(txt_path):
-    with open(txt_path, "r") as f:
-        return [line.strip() for line in f.readlines()]
+def build_caption_dataset(
+    image_ids, image_caption_seqs, word2idx, image_folder, transform
+):
+    image_filenames = []
+    caption_tensors = []
+    lengths = []
+
+    for img_id in image_ids:
+        if img_id in image_caption_seqs:
+            for seq in image_caption_seqs[img_id]:
+                image_filenames.append(img_id)
+                caption_tensors.append(torch.tensor(seq))
+                lengths.append(len(seq))
+
+    padded_seqs = pad_sequence(
+        caption_tensors, batch_first=True, padding_value=word2idx["<pad>"]
+    )
+    lengths_tensor = torch.tensor(lengths)
+
+    dataset = CaptionDataset(
+        image_folder=image_folder,
+        captions_tensor=padded_seqs,
+        lengths_tensor=lengths_tensor,
+        image_filenames=image_filenames,
+        transform=transform,
+    )
+    return dataset
